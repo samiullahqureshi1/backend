91a284afc86e7ded4e565823488636d625f85cdf
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-07-29 09:01:55 -0700

    new changes

--- Routes/email.js
+++ Routes/email.js
@@ -2,4 +2,5 @@ import email from "../controller/email.js";
 import express from 'express'
 const emailRouter=express.Router()
 emailRouter.post('/',email.addEmail)
+emailRouter.post('/mail',email.sendWithNodemailer)
 export default emailRouter;
--- controller/email.js
+++ controller/email.js
@@ -1,5 +1,6 @@
 import { emailModel } from "../model/email.js";
 import sgMail from '@sendgrid/mail'
+import nodemailer from 'nodemailer';
 
 const sendGrigKey=process.env.SENDGRID_KEY
 
@@ -16,13 +17,42 @@ const addEmail=(req,res)=>{
     }
     try {
         sgMail.send(message);
+        const email=new emailModel({to,subject,text,html})
+        email.save()
         res.status(201).send('email sent successfully')
     } catch (error) {
         console.log(error.message)
     }
 }
 
+const sendWithNodemailer = async (req, res) => {
+    try {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+  
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: '"sanawish" <sanawish146@gmail.com>',
+        subject: 'Hello World',
+        text: 'Hello World',
+        html: '<b>Hello World</b>',
+      };
+  
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+      res.json(info);
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ error: 'Error sending email' });
+    }
+  };
 
 export default {
-    addEmail
+    addEmail,
+    sendWithNodemailer,
 }
